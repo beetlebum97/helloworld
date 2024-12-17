1a19dd925c7d9a90c64c382f1fdefcab7114a5da
beetlebum97 <vazmadrid@gmail.com>
2024-12-17 20:18:21 +0100

    feature_fix_racecond

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -2,18 +2,34 @@ import http.client
 import os
 import unittest
 from urllib.request import urlopen
-
+import time
 import pytest
+import socket
 
 BASE_URL = "http://localhost:5000"
 BASE_URL_MOCK = "http://localhost:9090"
-DEFAULT_TIMEOUT = 2  # in secs
+DEFAULT_TIMEOUT = 2  # en segundos
+PORT_CHECK_TIMEOUT = 15  # Tiempo máximo para esperar al puerto
+
+def wait_for_port(host: str, port: int, timeout: int):
+    """Espera hasta que el puerto esté disponible o expire el tiempo."""
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            if sock.connect_ex((host, port)) == 0:
+                print(f"El puerto {port} está disponible.")
+                return
+        print(f"Esperando al puerto {port}...")
+        time.sleep(1)
+    raise TimeoutError(f"El puerto {port} no está disponible después de {timeout} segundos.")
+
 
 @pytest.mark.api
 class TestApi(unittest.TestCase):
-    def setUp(self):
-        self.assertIsNotNone(BASE_URL, "URL no configurada")
-        self.assertTrue(len(BASE_URL) > 8, "URL no configurada")
+    @classmethod
+    def setUpClass(cls):
+        wait_for_port("localhost", 5000, PORT_CHECK_TIMEOUT)
+        wait_for_port("localhost", 9090, PORT_CHECK_TIMEOUT)
 
     def test_api_add(self):
         url = f"{BASE_URL}/calc/add/1/2"
@@ -34,6 +50,7 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.read().decode(), "8", "ERROR SQRT"
         )
+
 
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
